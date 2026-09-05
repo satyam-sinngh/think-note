309de9d9deeed2d14466f1fdaa27bcab49e57282
Satyam Singh <satyamsinghbgs90@gmail.com>
2026-09-05 17:38:53 +0530

    refactor: convert table query functions to class for note repository

--- backend/src/repository/note.repo.ts
+++ backend/src/repository/note.repo.ts
@@ -1,42 +1,47 @@
 import prisma from "../lib/prisma.js";
 import {NoteInput, NoteUpdateInput} from "../types/note.type.js";
 
-export const fetchNotes = async (userId: string) => {
-    return await prisma.note.findMany({
-        where: {
-            user: {
-                id: userId,
+export class NoteRepository {
+    async fetchNotes(userId: string) {
+        return await prisma.note.findMany({
+            where: {
+                user: {
+                    id: userId,
+                }
             }
-        }
-    });
+        });
+    }
+
+    async createNote(data: NoteInput) {
+        return await prisma.note.create({
+            data
+        })
+    };
+
+    async updateNote(input: NoteUpdateInput) {
+        return await prisma.note.update({
+            where: {
+                id: input.id,
+                userId: input.userId,
+            },
+            data: input.data
+        })
+    }
+
+    async deleteNote(id: string) {
+        return await prisma.note.delete({
+            where: {
+                id,
+            },
+        })
+    }
+
+    async fetchNoteById(id: string) {
+        return await prisma.note.findUnique({
+            where: {id: id},
+        })
+    }
+
 }
 
-export const createNote = async (data: NoteInput) => {
-    return await prisma.note.create({
-        data
-    })
-};
-
-export const updateNote = async (input: NoteUpdateInput) => {
-    return await prisma.note.update({
-        where: {
-            id: input.id,
-            userId: input.userId,
-        },
-        data: input.data
-    })
-}
-
-export const deleteNote = async (id: string) => {
-    return await prisma.note.delete({
-        where: {
-            id,
-        },
-    })
-}
-
-export const fetchNoteById = async (id: string) => {
-    return await prisma.note.findUnique({
-        where: {id: id},
-    })
-}
+export const noteRepository = new NoteRepository();
--- backend/src/services/note.service.ts
+++ backend/src/services/note.service.ts
@@ -1,18 +1,18 @@
 import {NoteInput, NoteUpdateInput} from "../types/note.type.js";
-import {createNote, deleteNote, fetchNoteById, fetchNotes, updateNote} from "../repository/note.repo.js";
+import {noteRepository} from "../repository/note.repo.js";
 import {AppError} from "../errors/AppError.js";
 
 export const addNote = async (noteInput: NoteInput) => {
-    return await createNote(noteInput);
+    return await noteRepository.createNote(noteInput);
 }
 
 export const getUserNotes = async (userId: string) => {
-    return await fetchNotes(userId);
+    return await noteRepository.fetchNotes(userId);
 }
 
 export const getNoteById = async (noteId: string) => {
 
-    const note = await fetchNoteById(noteId);
+    const note = await noteRepository.fetchNoteById(noteId);
     if (!note) {
         throw new AppError("Note Note Found!", 404);
     }
@@ -20,7 +20,7 @@ export const getNoteById = async (noteId: string) => {
 }
 
 export const updateNoteService = async (noteInput: NoteUpdateInput) => {
-    const note = await updateNote(noteInput);
+    const note = await noteRepository.updateNote(noteInput);
     if (!note) {
         throw new AppError("Note Note Found!", 404);
     }
@@ -28,9 +28,9 @@ export const updateNoteService = async (noteInput: NoteUpdateInput) => {
 }
 
 export const deleteNoteService = async (noteId: string) => {
-    const note = await fetchNoteById(noteId);
+    const note = await noteRepository.fetchNoteById(noteId);
     if (!note) {
         throw new AppError("Note Note Found!", 404);
     }
-    return await deleteNote(noteId);
+    return await noteRepository.deleteNote(noteId);
 }
